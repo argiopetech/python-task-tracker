9471ac12c9d59da3cdd0be55cee5b584a81e989d
Elliot Robinson <elliot.robinson@argiopetech.com>
2022-10-20 08:22:45 -0400

    Initial commit with requirements and __main__ boilerplate

---
+++ task_tracker.py
@@ -0,0 +1,18 @@
+# Requirements:
+#   Text User Interface
+#   Read Evaluate Print loop
+#   Add Tasks
+#   Delete Tasks when complete
+#   list current tasks in the order they were added
+#   Persist across runs (load in multiple terminals)  (chapter 7.2p
+#   every user gets their own list
+#   editable storage format
+#   all tasks wil be entered by the user
+#   user friendly
+#   report # of tasks
+
+
+
+
+if __name__ == "__main__":
+    print("Do Something")
